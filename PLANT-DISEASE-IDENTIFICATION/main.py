38bc45d165a890b17e550f0ea8a0fdd7a6fe61a2
Akshat Pundlik <akshat.pundlik7@gmail.com>
2025-10-12 01:26:22 +0530

    Update main.py

--- PLANT-DISEASE-IDENTIFICATION/main.py
+++ PLANT-DISEASE-IDENTIFICATION/main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_plant_disease_model.keras")
+    # Update line 5 in main.py to load the directory
+model = tf.keras.models.load_model("saved_model_dir")
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr]) #convert single image to batch
@@ -54,4 +55,5 @@ elif(app_mode=="DISEASE RECOGNITION"):
                       'Tomato___healthy']
 
         st.success("Model is Predicting it's a {}".format(class_name[result_index]))
+
 
