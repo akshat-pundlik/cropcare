d58d8a9d8946fba313cfbd45b2fce9859bcf6c16
Akshat Pundlik <akshat.pundlik7@gmail.com>
2025-10-31 01:29:58 +0530

    Update main.py

--- PLANT-DISEASE-IDENTIFICATION/main.py
+++ PLANT-DISEASE-IDENTIFICATION/main.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 print(os.system('pwd'))
 print(os.system('ls'))
-print(os.system("ls./PLANT-DISEASE-IDENTIFICATION"))
+print(os.system("ls ./PLANT-DISEASE-IDENTIFICATION"))
 def model_prediction(test_image):
     model = tf.keras.models.load_model("PLANT-DISEASE-IDENTIFICATION/trained_plant_disease_model.keras")
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
@@ -57,6 +57,7 @@ elif(app_mode=="DISEASE RECOGNITION"):
                     'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
                       'Tomato___healthy']
         st.success("Model is Predicting it's a {}".format(class_name[result_index]))
+
 
 
 
