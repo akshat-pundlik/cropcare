e54257c73c4d3be1c1538573f6d80c4c55ee2b35
Akshat Pundlik <akshat.pundlik7@gmail.com>
2025-10-30 23:58:12 +0530

    Update main.py

--- PLANT-DISEASE-IDENTIFICATION/main.py
+++ PLANT-DISEASE-IDENTIFICATION/main.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st 
 import tensorflow as tf
 import numpy as np
 def model_prediction(test_image):
@@ -16,9 +16,9 @@ image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
 input_arr = tf.keras.preprocessing.image.img_to_array(image)
 input_arr = np.array([input_arr]) #convert single image to batch
 predictions = model.predict(input_arr)
-return np.argmax(predictions) #return index of max element
+    return np.argmax(predictions) #return index of max element
 
-#Sidebar
+#Sidebar   
 st.sidebar.title("AgriSens")
 app_mode = st.sidebar.selectbox("Select Page",["HOME","DISEASE RECOGNITION"])
 #app_mode = st.sidebar.selectbox("Select Page",["Home","About","Disease Recognition"])
@@ -62,6 +62,7 @@ elif(app_mode=="DISEASE RECOGNITION"):
                     'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
                       'Tomato___healthy']
         st.success("Model is Predicting it's a {}".format(class_name[result_index]))
+
 
 
 
